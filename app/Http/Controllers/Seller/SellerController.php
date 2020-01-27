bbf62e7536342018e3cbb69c6ba45a6f075c171e
r3nyou <renyou815@gmail.com>
2020-01-27 16:07:15 +0800

    fix bug

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -15,7 +15,7 @@ class SellerController extends ApiController
      */
     public function index()
     {
-        $sellers = Seller::has('products');
+        $sellers = Seller::has('products')->get();
 
         return $this->showAll($sellers);
     }
